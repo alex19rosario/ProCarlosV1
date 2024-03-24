e15215ab72b617744fa8728e49dc00d2cc4beaf6
Carlos Rosario <alexrosario7130492@gmail.com>
2024-03-24 14:50:28 -0400

    Adding College Application Services

---
+++ aspnet-core/src/ProCarlosV1.Application/Colleges/CollegeAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using ProCarlosV1.Colleges.Dto;
+using ProCarlosV1.Models;
+
+namespace ProCarlosV1.Colleges
+{
+    public class CollegeAppService : AsyncCrudAppService<College, CollegeDto, int, PagedCollegeResultRequestDto, CreateCollegeDto, UpdateCollegeDto>, ICollegeAppService
+    {
+        public CollegeAppService(IRepository<College, int> repository) : base(repository)
+        {
+
+        }
+    }
+
+}
---
+++ aspnet-core/src/ProCarlosV1.Application/Colleges/ICollegeAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Abp.Application.Services;
+using ProCarlosV1.Colleges.Dto;
+using ProCarlosV1.Students.Dto;
+
+namespace ProCarlosV1.Colleges
+{
+    public interface ICollegeAppService : IAsyncCrudAppService<CollegeDto, int, PagedCollegeResultRequestDto, CreateCollegeDto, UpdateCollegeDto>
+    {
+
+    }
+}
