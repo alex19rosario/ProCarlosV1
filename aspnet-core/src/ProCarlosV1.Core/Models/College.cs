e31bbd1684ad2fc0efa676b4e76ac48134cb1f4f
Carlos Rosario <alexrosario7130492@gmail.com>
2024-03-24 14:38:42 -0400

    Adding College entity to the Model

---
+++ aspnet-core/src/ProCarlosV1.Core/Models/College.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+
+namespace ProCarlosV1.Models
+{
+    public class College : FullAuditedEntity<int>, IPassivable
+    {
+        public College()
+        {
+            this.IsActive = true;
+            this.CreationTime = DateTime.Now;
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public string GpsLatitude { get; set; }
+        public string Longitude { get; set; }
+        public string Email { get; set; }
+        public string Director { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
