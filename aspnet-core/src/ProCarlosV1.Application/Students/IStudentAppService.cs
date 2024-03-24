242ca626b517c8f8fa8dd6ab46bc8f3c0d2dcc7a
Carlos Rosario <alexrosario7130492@gmail.com>
2024-03-24 08:41:54 -0400

    Adding the Application Service

---
+++ aspnet-core/src/ProCarlosV1.Application/Students/IStudentAppService.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services;
+using ProCarlosV1.Students.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCarlosV1.Students
+{
+    public interface IStudentAppService : IAsyncCrudAppService<StudentDto, int, PagedStudentResultRequestDto, CreateStudentDto, UpdateStudentDto>
+    {
+
+    }
+}
---
+++ aspnet-core/src/ProCarlosV1.Application/Students/StudentAppService.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using ProCarlosV1.Models;
+using ProCarlosV1.Students.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCarlosV1.Students
+{
+    // [AbpAuthorize(PermissionNames.Pages_Students)]
+    public class StudentAppService : AsyncCrudAppService<Student, StudentDto, int, PagedStudentResultRequestDto, CreateStudentDto, UpdateStudentDto>, IStudentAppService
+    {
+        public StudentAppService
+        (
+            IRepository<Student, int> repository
+        )
+        : base(repository)
+        {
+
+        }
+
+    }
+}
