ac22c0c5125cc2c8b8abab47c14f36716b4f0afa
Carlos Rosario <alexrosario7130492@gmail.com>
2024-03-24 00:32:35 -0400

    Disabling Multi Tenancy feature

--- aspnet-core/src/ProCarlosV1.Core/ProCarlosV1Consts.cs
+++ aspnet-core/src/ProCarlosV1.Core/ProCarlosV1Consts.cs
@@ -8,7 +8,7 @@ namespace ProCarlosV1
 
         public const string ConnectionStringName = "Default";
 
-        public const bool MultiTenancyEnabled = true;
+        public const bool MultiTenancyEnabled = false;
 
 
         /// <summary>
