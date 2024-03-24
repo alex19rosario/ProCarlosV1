34225c6d25aef84a87f216406b618d4be9d6dea8
Carlos Rosario <alexrosario7130492@gmail.com>
2024-03-24 01:09:15 -0400

    Creating a new Entity and Adding it to DBcontext

---
+++ aspnet-core/src/ProCarlosV1.EntityFrameworkCore/Migrations/20240324044023_AddStudentEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProCarlosV1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
